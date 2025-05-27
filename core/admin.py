7e365d0c3462af3216a15ef87fbed9256d31b0b5
YeferRivera <yeferson_rivera69222@elpoli.edu.co>
2025-05-27 16:53:30 -0500

    Registar modelos en damin y crea super usuario

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Plato, Orden, Pago
 
-# Register your models here.
+admin.site.register(Plato)
+admin.site.register(Orden)
+admin.site.register(Pago)
